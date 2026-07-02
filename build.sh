b169588f4f858692abcd1bad68c126e7574222c6
zhenzhang <zhangzhen3121@gmail.com>
2026-07-02 22:12:21 +0800

    7-移植opensbi

--- build.sh
+++ build.sh
@@ -1,20 +1,57 @@
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 set -e
+# 获取当前脚本文件所在的目录
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+if [ ! -d "$SHELL_FOLDER/output" ]; then  
+mkdir $SHELL_FOLDER/output
+fi  
+
+cd qemu-8.0.2
+if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
+./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
+fi  
+make -j16$PROCESSORS
+make install
+
+
+# # 编译 lowlevelboot
 CROSS_PREFIX=riscv64-unknown-elf
-
-if [ ! -d "$SHELL_FOLDER/output/lowlevelboot" ];then
-mkdir -p "$SHELL_FOLDER/output/lowlevelboot"
-fi
-cd $SHELL_FOLDER/boot
-
+if [ ! -d "$SHELL_FOLDER/output/lowlevelboot" ]; then  
+mkdir $SHELL_FOLDER/output/lowlevelboot
+fi  
+cd  $SHELL_FOLDER/boot
 $CROSS_PREFIX-gcc -x assembler-with-cpp -c start.s -o $SHELL_FOLDER/output/lowlevelboot/start.o
-$CROSS_PREFIX-gcc -nostdlib -nostartfiles -T./boot.lds -Wl,-Map=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections $SHELL_FOLDER/output/lowlevelboot/start.o -o $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf
+$CROSS_PREFIX-gcc -nostartfiles -T./boot.lds -Wl,-Map=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections $SHELL_FOLDER/output/lowlevelboot/start.o -o $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf
 # 使用gnu工具生成原始的程序bin文件
 $CROSS_PREFIX-objcopy -O binary -S $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.bin
 # 使用gnu工具生成反汇编文件，方便调试分析（当然我们这个代码太简单，不是很需要）
 $CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf > $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.lst
 
-cd $SHELL_FOLDER/output/lowlevelboot
+
+
+#编译 opensbi
+if [ ! -d "$SHELL_FOLDER/output/opensbi" ]; then  
+mkdir $SHELL_FOLDER/output/opensbi
+fi  
+cd $SHELL_FOLDER/opensbi
+make CROSS_COMPILE=$CROSS_PREFIX- PLATFORM=quard_star
+cp -r $SHELL_FOLDER/opensbi/build/platform/quard_star/firmware/*.bin $SHELL_FOLDER/output/opensbi/
+
+# 生成sbi.dtb
+cd $SHELL_FOLDER/dts
+dtc -I dts -O dtb -o $SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb quard_star_sbi.dts
+
+# 合成firmware固件
+if [ ! -d "$SHELL_FOLDER/output/fw" ]; then  
+mkdir $SHELL_FOLDER/output/fw
+fi  
+cd $SHELL_FOLDER/output/fw
 rm -rf fw.bin
-dd of=fw.bin bs=1k count=32k if=/dev/zero
-dd of=fw.bin bs=1k conv=notrunc seek=0 if=lowlevel_fw.bin
+# 填充 32K的0
+dd of=fw.bin bs=1k count=32k if=/dev/zero   
+# # 写入 lowlevel_fw.bin 偏移量地址为 0
+dd of=fw.bin bs=1k conv=notrunc seek=0 if=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.bin
+# 写入 quard_star_sbi.dtb 地址偏移量为 512K，因此 fdt的地址偏移量为 0x80000
+dd of=fw.bin bs=1k conv=notrunc seek=512 if=$SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb
+# 写入 fw_jump.bin 地址偏移量为 2K*1K= 0x2000000，因此 fw_jump.bin的地址偏移量为  0x2000000
+dd of=fw.bin bs=1k conv=notrunc seek=2k if=$SHELL_FOLDER/output/opensbi/fw_jump.bin
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-$SHELL_FOLDER/qemu-8.0.2/output/qemu/bin/qemu-system-riscv64 \
-	-M quard-star \
-	-m 1G\
-	-smp 8 \
-	-bios none \
-	-drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/lowlevelboot/fw.bin \
-	-nographic --parallel none
+
+$SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
+-M quard-star \
+-m 1G \
+-smp 8 \
+-bios none \
+-drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/fw/fw.bin \
+-d in_asm -D qemu.log \
+-nographic --parallel none \
